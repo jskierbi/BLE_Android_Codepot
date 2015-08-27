b759a2dcdb62ab07f80a7d397d78eafabfcfac5f
Krzysiek <kwrobel1989@gmail.com>
2015-08-28 01:13:21 +0200

    BLE connection in library

---
+++ sensortaglibrary/src/main/java/com/zinno/sensortaglibrary/BleService.java
@@ -0,0 +1,132 @@
+package com.zinno.sensortaglibrary;
+
+import android.app.Service;
+import android.bluetooth.BluetoothGattService;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Looper;
+
+import com.zinno.sensortaglibrary.ble.BleManager;
+import com.zinno.sensortaglibrary.ble.BleServiceListener;
+
+import java.util.List;
+
+/**
+ * Service for managing connection and data communication with a GATT server hosted on a
+ * given Bluetooth LE device.
+ */
+public class BleService extends Service implements BleServiceListener {
+    @SuppressWarnings("UnusedDeclaration")
+    private final static String TAG = BleService.class.getSimpleName();
+
+    public class LocalBinder extends Binder {
+        public BleService getService() {
+            return BleService.this;
+        }
+    }
+
+    private final IBinder binder = new LocalBinder();
+
+    private final Handler uiThreadHandler = new Handler(Looper.getMainLooper());
+    private final BleManager bleManager = new BleManager();
+    private BleServiceListener serviceListener;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        bleManager.setServiceListener(this);
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return binder;
+    }
+
+    @Override
+    public boolean onUnbind(Intent intent) {
+        // After using a given device, you should make sure that BluetoothGatt.close() is called
+        // such that resources are cleaned up properly.  In this particular example, close() is
+        // invoked when the UI is disconnected from the Service.
+        bleManager.close();
+        return super.onUnbind(intent);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        bleManager.disconnect();
+        bleManager.close();
+    }
+
+    public BleManager getBleManager() {
+        return bleManager;
+    }
+
+    public void setServiceListener(BleServiceListener listener) {
+        serviceListener = listener;
+    }
+
+    /**
+     * Retrieves a list of supported GATT services on the connected device. This should be
+     * invoked only after {@code BluetoothGatt#discoverServices()} completes successfully.
+     *
+     * @return A {@code List} of supported services.
+     */
+    public List<BluetoothGattService> getSupportedGattServices(String address) {
+        return bleManager.getSupportedGattServices(address);
+    }
+
+    @Override
+    public void onConnected(final String deviceAddress) {
+        uiThreadHandler.post(new Runnable() {
+            @Override
+            public void run() {
+                if (serviceListener != null) {
+                    serviceListener.onConnected(deviceAddress);
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onDisconnected(final String deviceAddress) {
+        uiThreadHandler.post(new Runnable() {
+            @Override
+            public void run() {
+                if (serviceListener != null) {
+                    serviceListener.onDisconnected(deviceAddress);
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onServiceDiscovered(final String deviceAddress) {
+        uiThreadHandler.post(new Runnable() {
+            @Override
+            public void run() {
+                if (serviceListener != null) {
+                    serviceListener.onServiceDiscovered(deviceAddress);
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onDataAvailable(final String deviceAddress, final String serviceUuid, final String characteristicUuid,
+                                final String text, final Object data) {
+
+        uiThreadHandler.post(new Runnable() {
+            @Override
+            public void run() {
+                if (serviceListener != null) {
+                    serviceListener.onDataAvailable(deviceAddress, serviceUuid, characteristicUuid, text, data);
+                }
+            }
+        });
+    }
+
+}
---
+++ sensortaglibrary/src/main/java/com/zinno/sensortaglibrary/ble/BleGattExecutor.java
@@ -0,0 +1,125 @@
+package com.zinno.sensortaglibrary.ble;
+
+import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothGattCallback;
+import android.bluetooth.BluetoothGattCharacteristic;
+import android.bluetooth.BluetoothGattDescriptor;
+import android.bluetooth.BluetoothProfile;
+
+import java.util.LinkedList;
+
+/**
+ * Created by steven on 9/3/13.
+ */
+public class BleGattExecutor extends BluetoothGattCallback {
+
+    public static abstract class ServiceAction {
+
+
+        public enum ActionType {
+            NONE,
+            READ,
+            NOTIFY,
+            WRITE
+        }
+
+        public static final ServiceAction NULL = new ServiceAction(null, ActionType.NONE) {
+            @Override
+            public boolean execute(BluetoothGatt bluetoothGatt) {
+                // it is null action. do nothing.
+                return true;
+            }
+        };
+
+        private final ActionType type;
+
+        private BluetoothGatt gatt;
+
+        public ServiceAction(BluetoothGatt gatt, ActionType type) {
+            this.gatt = gatt;
+            this.type = type;
+        }
+
+        public BluetoothGatt getGatt() {
+            return gatt;
+        }
+
+        public ActionType getType() {
+            return type;
+        }
+
+        /**
+         * Executes action.
+         *
+         * @param bluetoothGatt
+         * @return true - if action was executed instantly. false if action is waiting for
+         * feedback.
+         */
+        public abstract boolean execute(BluetoothGatt bluetoothGatt);
+    }
+
+    private final LinkedList<ServiceAction> queue = new LinkedList<ServiceAction>();
+    private volatile ServiceAction currentAction;
+
+    public void executeNextAction() {
+        if (currentAction != null) {
+            return;
+        }
+
+        boolean next = !queue.isEmpty();
+        while (next) {
+            final ServiceAction action = queue.pop();
+            currentAction = action;
+            if (!action.execute(currentAction.getGatt()))
+                break;
+
+            currentAction = null;
+            next = !queue.isEmpty();
+        }
+    }
+
+    @Override
+    public void onDescriptorWrite(BluetoothGatt gatt, BluetoothGattDescriptor descriptor, int status) {
+        super.onDescriptorWrite(gatt, descriptor, status);
+
+        // wait for onCharacteristicWrite for write action before execution of any other actions
+        if (currentAction != null && currentAction.getType() == ServiceAction.ActionType.WRITE) {
+            return;
+        }
+
+        currentAction = null;
+        executeNextAction();
+    }
+
+    @Override
+    public void onCharacteristicWrite(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic, int status) {
+        super.onCharacteristicWrite(gatt, characteristic, status);
+
+        currentAction = null;
+        executeNextAction();
+    }
+
+    @Override
+    public void onCharacteristicRead(BluetoothGatt gatt,
+                                     BluetoothGattCharacteristic characteristic,
+                                     int status) {
+        // wait for onCharacteristicWrite for write action before execution of any other actions
+        if (currentAction != null && currentAction.getType() == ServiceAction.ActionType.WRITE) {
+            return;
+        }
+
+        currentAction = null;
+        executeNextAction();
+    }
+
+    @Override
+    public void onConnectionStateChange(BluetoothGatt gatt, int status, int newState) {
+        if (newState == BluetoothProfile.STATE_DISCONNECTED) {
+            for (ServiceAction serviceAction : queue) {
+                if (serviceAction.gatt.equals(gatt)) {
+                    queue.remove(serviceAction);
+                }
+            }
+        }
+    }
+}
